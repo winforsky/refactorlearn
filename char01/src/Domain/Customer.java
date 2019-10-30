98bf44042c88c7513fd100f994a2bebc28cb0a67
郑少朋 <911917@nd.com>
2019-10-30 19:27:27 +0800

    char01第三次重构，在自己的类内部使用atate或stractrate替代switch

--- char01/src/Domain/Customer.java
+++ char01/src/Domain/Customer.java
@@ -45,12 +45,17 @@ public class Customer {
         Enumeration<Rental> rentals = this._rentals.elements();
         String result = "Domain.Rental Record for "+ get_name()+"\n";
         while (rentals.hasMoreElements()){
+            //Extract Method 110 抽取方法
+            //当方法体中的代码功能没有连续性时可以考虑提取
             Rental rental = rentals.nextElement();
             //show figures for this rental
             result += "\t"+rental.get_movie().get_title()+"\t"+String.valueOf(rental.getCharge())+"\n";
         }
 
         //add footer lines
+        //Replace temp with Query 120  去除临时变量
+        //totalAmount在循环体内被赋值，把它迁移到查询函数中去
+        //frequentRenterPoints 同理
         result += "Amount owed is "+String.valueOf(getTotalCharge())+"\n";
         result +="You earned "+String.valueOf(getTotalFrequentRenterPoints())+"frequent renter points\n";
         return result;
--- char01/src/Domain/Movie.java
+++ char01/src/Domain/Movie.java
@@ -1,58 +1,111 @@
 package Domain;
 
+abstract class Price {
+    abstract int getPriceCode();
+    abstract double getCharge(int daysRented);
+    int getFrequentRenterPoints(int daysRented) {
+        return 1;
+    }
+}
+
+class ChildrenPrice extends Price {
+    @Override
+    int getPriceCode() {
+        return Movie.CHILDREN;
+    }
+
+    @Override
+    double getCharge(int daysRented) {
+        double thisAmount = 1.5;
+        if (daysRented >3){
+            thisAmount += (daysRented -3)*1.5;
+        }
+        return thisAmount;
+    }
+}
+
+class RegularPrice extends Price {
+    @Override
+    int getPriceCode() {
+        return Movie.REGULAR;
+    }
+
+    @Override
+    double getCharge(int daysRented) {
+        double thisAmount = 2;
+        if (daysRented >2){
+            thisAmount += (daysRented -2)*1.5;
+        }
+        return thisAmount;
+    }
+}
+
+class NewReleasePrice extends Price {
+    @Override
+    int getPriceCode() {
+        return Movie.NEW_RELEASE;
+    }
+
+    @Override
+    double getCharge(int daysRented) {
+        return daysRented *3;
+    }
+
+    @Override
+    int getFrequentRenterPoints(int daysRented) {
+        if (daysRented >1){
+            return 2;
+        }
+        return 1;
+    }
+}
+
 public class Movie {
-    public static final int CHILDRENS = 2;
+    public static final int CHILDREN = 2;
     public static final int REGULAR =0;
     public static final int  NEW_RELEASE = 1;
 
     private String _title;
-    private int _priceCode;
+    private Price _price;
 
-    public Movie(String _title, int _priceCode) {
-        this._title = _title;
-        this._priceCode = _priceCode;
+    public Movie(String title, int priceCode) {
+        this._title = title;
+        //self encapsulate field 171
+        set_priceCode(priceCode);
     }
 
     public int get_priceCode() {
-        return _priceCode;
+        return _price.getPriceCode();
     }
 
-    public void set_priceCode(int _priceCode) {
-        this._priceCode = _priceCode;
+    public void set_priceCode(int priceCode) {
+        //replace type code with state/strategy 227
+        switch (priceCode){
+            case CHILDREN:
+                _price= new ChildrenPrice();
+                break;
+            case REGULAR:
+                _price= new RegularPrice();
+                break;
+            case NEW_RELEASE:
+                _price= new NewReleasePrice();
+                break;
+            default:
+                throw new IllegalArgumentException("Incorrect Price Code");
+        }
     }
 
     public String get_title() {
         return _title;
     }
 
+    //move method 402
     public double getCharge(int daysRented) {
-        double thisAmount = 0;
-        switch (get_priceCode()){
-            case REGULAR:
-                thisAmount +=2;
-                if (daysRented >2){
-                    thisAmount += (daysRented -2)*1.5;
-                }
-                break;
-            case NEW_RELEASE:
-                    thisAmount += daysRented *3;
-
-                break;
-            case CHILDRENS:
-                thisAmount +=1.5;
-                if (daysRented >3){
-                    thisAmount += (daysRented -3)*1.5;
-                }
-                break;
-        }
-        return thisAmount;
+        //replace conditional with Polymorphism 255
+        return _price.getCharge(daysRented);
     }
 
     public int getFrequentRenterPoints(int daysRented) {
-        int frequentRenterPoints = 1;
-        if (get_priceCode() == NEW_RELEASE && daysRented >1){
-            frequentRenterPoints++;
-        }
-        return frequentRenterPoints;
+        return _price.getFrequentRenterPoints(daysRented);
     }
 }
